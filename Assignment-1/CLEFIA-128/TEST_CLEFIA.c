3864673f92510bc47818bda816771fc3096d3bed
Jayanth2209 <ee19b026@smail.iitm.ac.in>
2022-08-21 01:31:08 +0530

    Finished CLEFIA TEST

--- Assignment-1/CLEFIA-128/TEST_CLEFIA.c
+++ Assignment-1/CLEFIA-128/TEST_CLEFIA.c
@@ -11,11 +11,9 @@ void BytePut(const unsigned char *data, int bytelen)
 
 int main(void)
 {
-  const unsigned char skey[32] = {
+  const unsigned char skey[16] = {
     0xffU,0xeeU,0xddU,0xccU,0xbbU,0xaaU,0x99U,0x88U,
-    0x77U,0x66U,0x55U,0x44U,0x33U,0x22U,0x11U,0x00U,
-    0xf0U,0xe0U,0xd0U,0xc0U,0xb0U,0xa0U,0x90U,0x80U,
-    0x70U,0x60U,0x50U,0x40U,0x30U,0x20U,0x10U,0x00U
+    0x77U,0x66U,0x55U,0x44U,0x33U,0x22U,0x11U,0x00U
   };
   const unsigned char pt[16] = {
     0x00U,0x01U,0x02U,0x03U,0x04U,0x05U,0x06U,0x07U,
@@ -24,23 +22,21 @@ int main(void)
   unsigned char ct[16];
   unsigned char dst[16];
   unsigned char rk[8 * 26 + 16]; /* 8 bytes x 26 rounds(max) + whitening keys */
-  int r;
 
   printf("--- Test ---\n");
   printf("Plain Text:  "); BytePut(pt, 16);
-  printf("Secret Key:  "); BytePut(skey, 32);
+  printf("Secret Key:  "); BytePut(skey, 16);
 
   /* for 128-bit key */
   printf("--- CLEFIA-128 ---\n");
   /* encryption */
-  r = ClefiaKeySet(rk, skey, 128);
-  ClefiaEncrypt(dst, pt, rk, r);
-  printf("ciphertext: "); BytePut(dst, 16);
+  ClefiaKeySet128(rk, skey);
+  ClefiaEncrypt(rk, pt, ct);
+  printf("Cipher Text: "); BytePut(ct, 16);
   /* decryption */
-  ByteCpy(ct, dst, 16);
-  r = ClefiaKeySet(rk, skey, 128);
-  ClefiaDecrypt(dst, ct, rk, r);
-  printf("plaintext : "); BytePut(dst, 16);
+  ClefiaDecrypt(rk, ct, dst);
+  printf("Plain Text : "); BytePut(dst, 16);
 
   return 0;
+
 }
